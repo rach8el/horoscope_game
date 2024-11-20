39fad42fb3f416a789037cbbdcc0b67ca5e60e5f
student <student@Admins-MacBook-Pro.local>
2024-11-20 17:03:17 -0500

    first commit

---
+++ independent_projects/conditionals_game/horoscope.py
@@ -0,0 +1,48 @@
+print("Tell the witch what your zodiac sign is and learn your fortune for today...")
+
+player_answer = input("tell the witch your sign!")
+print("Your zodiac sign is: " + player_answer)
+
+
+if player_answer == 'aries':
+    print("an abundance of bats may swarm your home tonight")
+
+if player_answer == 'taurus':
+    print("whispering winds could shroud you at random tonight.")
+
+if player_answer == 'leo':
+    print("a tempting vampire may greet you outside tonight.")
+
+if player_answer == 'virgo':
+    print("black cats are sure to approach your way soon.")
+
+if player_answer == 'libra':
+    print("knocking on wood wont save you this time.")
+
+if player_answer == 'scorpio':
+    print("something may lurk in your closet... do you know what it is?")
+
+if player_answer == 'sagittarius':
+    print("if you eat from a knife, youll be angry for life.")
+
+if player_answer == 'capricorn':
+    print("dont fret if zombies are watering your garden.")
+
+if player_answer == 'aquarius':
+    print("a scarily sweet treat will spawn upon your porch at midnight.")
+
+if player_answer == 'pisces':
+    print("aliens are coming.")
+
+
+aries = ("an abundance of bats may swarm your home tonight.")
+taurus = ("whispering winds could shroud you at random tonight.")
+gemini = ("a minotaur wielding battle experience might challenge you soon.")
+leo = ("a prepossessing vampire may greet you outside tonight.")
+virgo = ("black cats are sure to approach your way soon.")
+libra = ("knocking on wood wont save you this time.")
+scorpio = ("something may lurk in your closet... do you know what it is?")
+sagittarius = ("if you eat from a knife, youll be angry for life.")
+capricorn = ("dont fret if zombies are watering your garden.")
+aquarius = ("a scarily sweet treat will spawn upon your porch at midnight.")
+pisces = ("aliens are coming.")
